2aba84503d8dc22e4ba925d77ba937b567153f07
Chetan Sarva <chetan@pixelcop.net>
2009-10-22 23:02:52 -0400

    moving right along

--- src/ruby/rack.rb
+++ src/ruby/rack.rb
@@ -1,44 +1,46 @@
 
 module Pixelcop
-    module Rack
+  module Web
+      
     class Application
       
-        # The main rack application call method.  This is the entry point from rack (and the webserver) 
-        # to your application.  
-        #
-        # ==== Parameters
-        # env<Hash>:: A rack request of parameters.  
-        #
-        # ==== Returns
-        # <Array>:: A rack response of [status<Integer>, headers<Hash>, body<String, Stream>]
-        #
-        # :api: private
-        def call(env) 
+        def call(env)         
+          dup.call!(env)
+        end
         
-            begin
-            
-                return   [
-    200,          # Status code
-    {             # Response headers
-      'Content-Type' => 'text/html',
-      'Content-Length' => '2',
-    },
-    ['hi']        # Response body
-  ]
-                
-                
-            
-                #return ::Merb::Dispatcher.handle(Merb::Request.new(env))
-            rescue Object => e
-                return [
-                    500, 
-                    {Merb::Const::CONTENT_TYPE => Merb::Const::TEXT_SLASH_HTML}, 
-                    e.message + Merb::Const::BREAK_TAG + e.backtrace.join(Merb::Const::BREAK_TAG)
-                    ]
-            end
-        
+        def call!(env)
+          
+          @request = Request.new(env)
+          @response = Response.new
+          
+          require 'yaml'
+          puts "te"
+          puts @request["hi"]
+      
+          begin
+          
+              return   [
+                        200,          # Status code
+                        {             # Response headers
+                          'Content-Type' => 'text/html',
+                          'Content-Length' => '2',
+                        },
+                        ['hi']        # Response body
+                       ]
+              
+              
+          
+              #return ::Merb::Dispatcher.handle(Merb::Request.new(env))
+          rescue Object => e
+              return [
+                  500, 
+                  {Merb::Const::CONTENT_TYPE => Merb::Const::TEXT_SLASH_HTML}, 
+                  e.message + Merb::Const::BREAK_TAG + e.backtrace.join(Merb::Const::BREAK_TAG)
+                  ]
+          end
+          
         end
 
     end # Application
-    end # Rack
+  end # Rack
 end # Pixelcop
--- src/ruby/server.ru
+++ src/ruby/server.ru
@@ -1,12 +1,24 @@
 
+# rackup configuration
+# configures rack and some basic rack middleware
+# then ties the webserver (any webserver) to our app 
+
 __DIR__ = File.dirname(__FILE__)
 
 require __DIR__ + '/rack'
 
 use Rack::CommonLogger
 use Rack::Static, :urls => ["/css", "/images", "/favicon.ico"], :root => __DIR__ + '/../web'
+use Rack::Reloader
 use Rack::ContentLength
 
+# setup autoloads
 
+module Pixelcop
+  module Web
+    autoload :Request, "http"
+    autoload :Response, "http"
+  end
+end
 
-run Pixelcop::Rack::Application.new
+run Pixelcop::Web::Application.new
