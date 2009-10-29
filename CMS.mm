b3174e30b0feacf0857c3797b879603ae5c0ab3d
Chetan Sarva <chetan@pixelcop.net>
2009-10-29 19:40:31 -0400

    updated docs

--- CMS.mm
+++ CMS.mm
@@ -1,28 +1,28 @@
 <map version="0.8.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1248061863895" ID="Freemind_Link_744823024" MODIFIED="1248062804735" TEXT="CMS">
-<font NAME="Verdana" SIZE="12"/>
+<font NAME="Dialog" SIZE="12"/>
 <icon BUILTIN="gohome"/>
 <node CREATED="1248061949365" HGAP="27" ID="_" MODIFIED="1248062777026" POSITION="right" TEXT="Content" VSHIFT="-43">
-<font NAME="Verdana" SIZE="12"/>
+<font NAME="Dialog" SIZE="12"/>
 <node CREATED="1248062424472" ID="Freemind_Link_354616168" MODIFIED="1248062777026" TEXT="Asset?">
-<font NAME="Verdana" SIZE="12"/>
+<font NAME="Dialog" SIZE="12"/>
 <icon BUILTIN="help"/>
 </node>
 <node CREATED="1248062699643" ID="Freemind_Link_56401549" MODIFIED="1248062777026" TEXT="Represents a single content &quot;node&quot;">
-<font NAME="Verdana" SIZE="12"/>
+<font NAME="Dialog" SIZE="12"/>
 </node>
 <node CREATED="1248062640772" HGAP="36" ID="Freemind_Link_374681180" MODIFIED="1248062777026" TEXT="Types" VSHIFT="23">
-<font NAME="Verdana" SIZE="12"/>
+<font NAME="Dialog" SIZE="12"/>
 <node CREATED="1248062102763" ID="Freemind_Link_705460998" MODIFIED="1248062777025" TEXT="Text">
-<font NAME="Verdana" SIZE="12"/>
+<font NAME="Dialog" SIZE="12"/>
 <node CREATED="1248142665093" HGAP="21" ID="Freemind_Link_791905156" MODIFIED="1248142670739" TEXT="wiki" VSHIFT="-11"/>
 </node>
 <node CREATED="1248062113011" ID="Freemind_Link_862683979" MODIFIED="1248062777025" TEXT="Comment">
-<font NAME="Verdana" SIZE="12"/>
+<font NAME="Dialog" SIZE="12"/>
 </node>
 <node CREATED="1248062110235" ID="Freemind_Link_1937736212" MODIFIED="1248062777025" TEXT="Photo">
-<font NAME="Verdana" SIZE="12"/>
+<font NAME="Dialog" SIZE="12"/>
 </node>
 <node CREATED="1248142587802" ID="Freemind_Link_1165859052" MODIFIED="1248142589801" TEXT="Remote">
 <node CREATED="1248142590179" ID="Freemind_Link_1250097798" MODIFIED="1248142597983" TEXT="iframe"/>
@@ -36,41 +36,43 @@
 <node CREATED="1248062903682" ID="Freemind_Link_263997082" MODIFIED="1248062953284" TEXT="Each type provides" VSHIFT="31">
 <node CREATED="1248062915324" ID="Freemind_Link_1059582380" MODIFIED="1248062918857" TEXT="Storage"/>
 <node CREATED="1248062920469" ID="Freemind_Link_1969907781" MODIFIED="1248062922465" TEXT="Retrieval"/>
-<node CREATED="1248062923214" ID="Freemind_Link_1888405640" MODIFIED="1248062927233" TEXT="Basic manipulation"/>
+<node CREATED="1248062923214" ID="Freemind_Link_1888405640" MODIFIED="1256587319423" TEXT="Basic manipulation">
+<icon BUILTIN="help"/>
+</node>
 </node>
 </node>
 </node>
 <node CREATED="1248062053625" ID="Freemind_Link_1315519500" MODIFIED="1248062777024" POSITION="right" TEXT="Organization">
-<font NAME="Verdana" SIZE="12"/>
+<font NAME="Dialog" SIZE="12"/>
 <node CREATED="1248062620764" HGAP="53" ID="Freemind_Link_1432311953" MODIFIED="1248062777024" TEXT="Types" VSHIFT="27">
-<font NAME="Verdana" SIZE="12"/>
+<font NAME="Dialog" SIZE="12"/>
 <node CREATED="1248062265009" ID="Freemind_Link_699780362" MODIFIED="1248062777024" TEXT="Flat">
-<font NAME="Verdana" SIZE="12"/>
+<font NAME="Dialog" SIZE="12"/>
 </node>
 <node CREATED="1248062059097" ID="Freemind_Link_1185332471" MODIFIED="1248062777024" TEXT="Time">
-<font NAME="Verdana" SIZE="12"/>
+<font NAME="Dialog" SIZE="12"/>
 </node>
 <node CREATED="1248062083450" ID="Freemind_Link_449567165" MODIFIED="1248062777023" TEXT="Tree">
-<font NAME="Verdana" SIZE="12"/>
+<font NAME="Dialog" SIZE="12"/>
 </node>
 <node CREATED="1248062088075" HGAP="24" ID="Freemind_Link_343077865" MODIFIED="1248062777023" TEXT="Tags" VSHIFT="1">
-<font NAME="Verdana" SIZE="12"/>
+<font NAME="Dialog" SIZE="12"/>
 </node>
 </node>
 <node CREATED="1248062672322" HGAP="28" ID="Freemind_Link_146658410" MODIFIED="1248062777023" TEXT="API" VSHIFT="41">
-<font NAME="Verdana" SIZE="12"/>
+<font NAME="Dialog" SIZE="12"/>
 <node CREATED="1248062676747" ID="Freemind_Link_605250393" MODIFIED="1248062777021" TEXT="Works on a set of nodes">
-<font NAME="Verdana" SIZE="12"/>
+<font NAME="Dialog" SIZE="12"/>
 </node>
 <node CREATED="1248062683107" ID="Freemind_Link_418302679" MODIFIED="1248062968291" TEXT="Can be stacked/layered">
-<font NAME="Verdana" SIZE="12"/>
+<font NAME="Dialog" SIZE="12"/>
 </node>
 </node>
 </node>
 <node CREATED="1248062225352" HGAP="37" ID="Freemind_Link_1243199693" MODIFIED="1248062886585" POSITION="left" TEXT="Application" VSHIFT="-33">
-<font NAME="Verdana" SIZE="12"/>
+<font NAME="Dialog" SIZE="12"/>
 <node CREATED="1248062228680" ID="Freemind_Link_571191265" MODIFIED="1248062856712" TEXT="Combines Content + Organization" VSHIFT="-25">
-<font NAME="Verdana" SIZE="12"/>
+<font NAME="Dialog" SIZE="12"/>
 </node>
 </node>
 <node CREATED="1248062820105" HGAP="36" ID="Freemind_Link_1473364698" MODIFIED="1248062853672" POSITION="left" TEXT="Routes" VSHIFT="38">
