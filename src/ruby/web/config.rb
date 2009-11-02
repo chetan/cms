8580860602d649b4218f8369886fa433893c1c1b
Chetan Sarva <chetan@pixelcop.net>
2009-11-02 16:43:40 -0500

    config class. not sure if this will stay

---
+++ src/ruby/web/config.rb
@@ -0,0 +1,18 @@
+
+module Pixelcop
+    
+    module Web
+        
+        class Config
+            
+            class << self
+                
+                attr_accessor :views_path
+                
+            end # self
+            
+        end # Config
+        
+    end # Web
+    
+end # Pixelcop
