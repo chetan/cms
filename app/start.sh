7b763e6dcc2748196e2f890afacd12994ac3be98
Chetan Sarva <chetan@pixelcop.net>
2009-11-03 11:42:49 -0500

    added alternative ways to start server

--- app/start.sh
+++ app/start.sh
@@ -1,2 +1,8 @@
 DIR=`dirname $0`
-thin start -R $DIR/server.ru
+
+# can use any of these to start
+
+#thin start -R $DIR/server.ru
+#rackup -s webrick $DIR/server.ru
+#rackup -s mongrel $DIR/server.ru
+rackup -s thin $DIR/server.ru
