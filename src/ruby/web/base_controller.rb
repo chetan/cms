7c3d68187ceeceaff827d73cad7c5b62061c280b
Chetan Sarva <chetan@pixelcop.net>
2009-11-10 21:41:19 -0500

    added redirect helper

--- src/ruby/web/base_controller.rb
+++ src/ruby/web/base_controller.rb
@@ -8,6 +8,11 @@ module Pixelcop
             attr_accessor :response
             def_init :request, :response
             
+            # quick helper method
+            def redirect(target, status = 302)
+                @response.redirect(target, status)
+            end
+
         end # BaseController
         
     end # Web
