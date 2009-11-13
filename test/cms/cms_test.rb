34aead45f925c6d46db3c8f03e457f232bf0f038
Chetan Sarva <chetan@pixelcop.net>
2009-11-12 19:00:31 -0500

    stub for CMS module tests

---
+++ test/cms/cms_test.rb
@@ -0,0 +1,16 @@
+
+require "monkey"
+require "cms"
+
+require "test/unit"
+
+Pixelcop::CMS::Text
+
+class CMS_Test < Test::Unit::TestCase
+   
+    def test_register_type
+        
+    end
+    
+end
+
