909880c49dbddbdca1545da93b2cc8551dcfe195
Chetan Sarva <chetan@pixelcop.net>
2009-11-17 13:01:45 -0500

    modified Route class to be storable

--- lib/cms/web/route.rb
+++ lib/cms/web/route.rb
@@ -1,25 +1,47 @@
 
 module Pixelcop
+    
     module CMS
+        
         module Web
             
-        module ContentRoute
-            attr_accessor :content_id
+            module ContentRoute
+                attr_accessor :content_id
             
-            def content
-                return nil if @content_id.blank?
-                # lookup content
+                def content
+                    return nil if @content_id.blank?
+                    # lookup content
                 
+                end
             end
-        end
-            
-        class Route < Pixelcop::Web::Route
+        
+        end # Web
+        
+    end # CMS
+    
+    module Web
+        
+        # extend Route classes to make them storable
+        
+        class Route
             
             include Pixelcop::CMS::MongoStorage
-            @@collection_name = "route"
+            self.collection_name = "routes"
+            
+            attr_reader :id
+            
+            def type
+                self.class.name
+            end
+            
+            key :id, :type, :pattern, :controller, :action
             
         end # Route
         
-        end # Web
-    end # CMS
+        class PrettyRoute
+            key :mod
+        end
+        
+    end # Web
+    
 end # Pixelcop
---
+++ test/cms/web/route_test.rb
@@ -0,0 +1,42 @@
+
+require "monkey"
+require "cms"
+require "web"
+
+require "cms/mongo_setup"
+require "cms/web/route"
+
+# Pixelcop::Web::Route.connection = Mongo::Connection.new("localhost")
+# Pixelcop::Web::Route.database = 'mycms_tests'
+
+require "test/unit"
+
+class Route_Test < Test::Unit::TestCase
+    
+    def teardown
+        Pixelcop::Web::Route.collection.drop
+        Pixelcop::Web::Route.collection = nil
+    end
+    
+    def test_save_route
+       
+        route = Pixelcop::Web::Route.new("/world", "MyApp::HelloWorldController", "index")
+        assert_nil(route.id)
+        route.save
+        assert_not_nil(route.id)        
+        return route
+    end
+    
+    def test_load_route
+        route = test_save_route
+        # now try to load it
+        loaded = Pixelcop::Web::Route.load(route.id)
+        assert_not_nil(loaded)
+    end
+    
+    def test_save_prettyroute
+        route = Pixelcop::Web::PrettyRoute.new("/:controller/:action", "MyApp")
+        route.save
+    end
+    
+end
