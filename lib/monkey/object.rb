024f5b83a6db5baddd36d32a313d14f16783219b
Chetan Sarva <chetan@pixelcop.net>
2009-11-17 12:53:49 -0500

    moved read/write attr to Object class

--- lib/monkey/object.rb
+++ lib/monkey/object.rb
@@ -1,16 +1,28 @@
 
 class Object
+
+    def self.read_attr(name)
+        begin
+            return class_variable_get("@@#{name}")
+        rescue NameError => e
+            return nil
+        end
+    end
+    
+    def self.write_attr(name, val)
+        class_variable_set("@@#{name}", val)
+    end
    
     def read_attr(name)
-        instance_variable_get("@#{name}")
+        respond_to?(name.to_sym) ? send(name.to_sym) : instance_variable_get("@#{name}")
     end
     
     def write_attr(name, val)
-       instance_variable_set("@#{name}", val)
+        instance_variable_set("@#{name}", val)
     end
    
-   def blank?
-       respond_to?(:empty?) ? empty? : !self
-   end
+    def blank?
+        respond_to?(:empty?) ? empty? : !self
+    end
     
 end
